49d89c20be8f1d058a13a8b3f279079dc4eb040f
Brandon Kraft <public@brandonkraft.com>
2012-12-14 08:57:51 -0600

    Forgot what I did last night, but it was something...

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ require_once( get_template_directory() . '/lib/init.php' );
 
 /** Child theme (do not remove) */
 define( 'CHILD_THEME_NAME', 'Sample Child Theme' );
-define( 'CHILD_THEME_URL', 'http://www.studiopress.com/themes/genesis' );
+define( 'CHILD_THEME_URL', 'http://www.coffeaweb.com' );
 
 /** Add Viewport meta tag for mobile browsers */
 add_action( 'genesis_meta', 'add_viewport_meta_tag' );
@@ -20,3 +20,13 @@ add_theme_support( 'genesis-custom-header', array( 'width' => 960, 'height' => 1
 
 /** Add support for 3-column footer widgets */
 add_theme_support( 'genesis-footer-widgets', 3 );
+
+/** Filter Footer Crds */
+add_filter( 'genesis_footer_creds_text', 'kraft_footer_creds_text');
+function kraft_footer_creds_text() {
+	echo '<div class="creds"><p>';
+	echo 'Copyright &copy; ';
+	echo date(Y);
+	echo ' &middot; Development by <a href="http://coffeaweb.com">Coffea Web Services &middot; Built on <a href="http://wordpress.org">WordPress</a> &amp; <a href="http://brandonkraft.com/go/studiopress/">Genesis</a>.';
+	echo '</p></div>':
+}
