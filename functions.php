a0ce0af8d265fa494de4d5d1ffb38df5a6729710
Brandon Kraft <web-comments@brandonkraft.com>
2013-02-24 22:36:00 -0600

    Revert "Initial commit of G1.9 changes"
    This reverts commit 88bdbb8dc45eca3d245fd1cf4e4853850b750dc4.

--- functions.php
+++ functions.php
@@ -1,30 +1,27 @@
 <?php
-// Start the engine
+/** Start the engine */
 require_once( get_template_directory() . '/lib/init.php' );
 
-// Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
-define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+/** Child theme (do not remove) */
+define( 'CHILD_THEME_NAME', 'Sample Child Theme' );
+define( 'CHILD_THEME_URL', 'http://www.coffeaweb.com' );
 
-// Add Viewport meta tag for mobile browsers
-add_action( 'genesis_meta', 'sample_viewport_meta_tag' );
-function sample_viewport_meta_tag() {
-	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
+/** Add Viewport meta tag for mobile browsers */
+add_action( 'genesis_meta', 'add_viewport_meta_tag' );
+function add_viewport_meta_tag() {
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
 }
 
-// Add support for custom background
-add_theme_support( 'custom-background' );
+/** Add support for custom background */
+add_custom_background();
 
-// Add support for custom header
-add_theme_support( 'genesis-custom-header', array(
-	'width' => 1152,
-	'height' => 120
-) );
+/** Add support for custom header */
+add_theme_support( 'genesis-custom-header', array( 'width' => 960, 'height' => 100 ) );
 
-// Add support for 3-column footer widgets
+/** Add support for 3-column footer widgets */
 add_theme_support( 'genesis-footer-widgets', 3 );
 
-// Filter Footer Crds
+/** Filter Footer Crds */
 add_filter( 'genesis_footer_creds_text', 'kraft_footer_creds_text');
 function kraft_footer_creds_text() {
 	echo '<div class="creds"><p>';
