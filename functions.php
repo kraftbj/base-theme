fb2312f53cd94831308d78440787709920a55928
Brandon Kraft <public@brandonkraft.com>
2013-05-15 18:20:12 -0500

    Initial commit of g2-beta1 theme

--- functions.php
+++ functions.php
@@ -1,32 +1,25 @@
 <?php
-/** Start the engine */
-include_once( get_template_directory() . '/lib/init.php' );
+//* Start the engine
+require_once( get_template_directory() . '/lib/init.php' );
 
-/** Child theme (do not remove) */
-define( 'CHILD_THEME_NAME', 'Your Custom Theme' );
-define( 'CHILD_THEME_URL', 'http://www.coffeaweb.com' );
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
 
-/** Add Viewport meta tag for mobile browsers */
-add_action( 'genesis_meta', 'add_viewport_meta_tag' );
-function add_viewport_meta_tag() {
-    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
+//* Enqueue Lato Google font
+add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
+function genesis_sample_google_fonts() {
+	wp_enqueue_style( 'google-font', '//fonts.googleapis.com/css?family=Lato:700', array(), PARENT_THEME_VERSION );
 }
 
-/** Add support for custom background */
-add_custom_background();
+//* Add HTML5 markup structure
+add_theme_support( 'genesis-html5' );
 
-/** Add support for custom header */
-add_theme_support( 'genesis-custom-header', array( 'width' => 960, 'height' => 100 ) );
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
 
-/** Add support for 3-column footer widgets */
+//* Add support for custom background
+add_theme_support( 'custom-background' );
+
+//* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
-
-/** Filter Footer Crds */
-add_filter( 'genesis_footer_creds_text', 'kraft_footer_creds_text');
-function kraft_footer_creds_text() {
-	echo '<div class="creds"><p>';
-	echo 'Copyright &copy; ';
-	echo date(Y);
-	echo ' &middot; Development by <a href="http://coffeaweb.com">Coffea Web Services &middot; Built on <a href="http://wordpress.org">WordPress</a> &amp; <a href="http://brandonkraft.com/go/studiopress/">Genesis</a>.';
-	echo '</p></div>';
-}
